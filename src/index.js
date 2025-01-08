0b24ee184f2895b76139cb2c9f0802b7bae32841
Marine Crouzet <marine.m.crouzet@gmail.com>
2025-01-08 15:27:45 +0100

    Added JS Timeout so that once button copy email clicked, it displays the message -copied- and after 2 seconds, resets to original message

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 // JS : Animating text using JS : https://www.shecodes.io/athena/2225-animating-text-with-javascript
 
-// function scrollleft() {
-//   document.getElementById("scrolling-text").style.left = "100%";
-//   setTimeout(scrollleft, 25);
-// }
-// scrollleft();
+// Copying the value of the button "Copy Email" in her section
 
 let emailValue = () => {
   let inputValue = document.getElementById("myInput");
   navigator.clipboard.writeText(inputValue.value);
   console.log(inputValue.value);
+
+  const getCopyEmail = document.getElementById("copy-email");
+  const originalText = getCopyEmail.innerHTML;
+  getCopyEmail.innerHTML = "copied ✔️";
+
+  const reset = () => {
+    getCopyEmail.innerHTML = originalText;
+  };
+
+  setTimeout(reset, 2000);
 };
 
 const getCopyEmail = document.getElementById("copy-email");
