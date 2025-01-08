105470f8687ecae1da17555e9258f7589eb1c7ff
Marine Crouzet <marine.m.crouzet@gmail.com>
2025-01-08 14:52:33 +0100

    Added the JS for clicking a button and getting my email address. Changed slighly the design of the hero section. Added the scroll down icon

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 // JS : Animating text using JS : https://www.shecodes.io/athena/2225-animating-text-with-javascript
 
+// function scrollleft() {
+//   document.getElementById("scrolling-text").style.left = "100%";
+//   setTimeout(scrollleft, 25);
+// }
+// scrollleft();
+
+let emailValue = () => {
+  let inputValue = document.getElementById("myInput");
+  navigator.clipboard.writeText(inputValue.value);
+  console.log(inputValue.value);
+};
+
+const getCopyEmail = document.getElementById("copy-email");
+getCopyEmail.addEventListener("click", emailValue);
