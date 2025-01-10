8ced1c49c070cea349291a26f45d1bd0046e9fde
Marine Crouzet <marine.m.crouzet@gmail.com>
2025-01-10 17:23:10 +0100

    using JS, toggling the responsive nav to open and close, when clicking on toggle and close buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 // JS : Animating text using JS : https://www.shecodes.io/athena/2225-animating-text-with-javascript
+// / * -------- Responsive - toggle responsive nav menu ---------------------------
+let openResponsiveNav = () => {
+  let displayResponsieMenu = document.getElementById("display-responsive-menu");
+  displayResponsieMenu.classList.toggle("hidden");
+};
 
-// * Copying the value of the button "Copy Email", for end-users
+const openResponsiveMenuBtn = document.querySelector(
+  "#responsive-nav-menu-btn"
+);
+console.log(openResponsiveMenuBtn);
+openResponsiveMenuBtn.addEventListener("click", openResponsiveNav);
+
+// * -------- Responsive - close responsive nav menu ---------------------------
+let displayResponsiveNav = () => {
+  let closeResponsieMenu = document.getElementById("display-responsive-menu");
+  closeResponsieMenu.classList.toggle("hidden");
+};
+
+const menuBtn = document.querySelector("#mobile-nav-btn");
+menuBtn.addEventListener("click", displayResponsiveNav);
+
+// * -------- Copying the value of the button "Copy Email", for end-users -------------
 let emailValue = (event) => {
   let button = event.target;
   console.log(button);
